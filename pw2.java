110ad4165513c5664e4fd40df23a64ae2bed2b54
ultimatecoder <sonukumar764499@gmail.com>
2023-01-28 16:52:30 +0530

    first commit

---
+++ pw2.java
@@ -0,0 +1,17 @@
+package InputJava;
+import java.util.Scanner;
+public class areaofcirle
+{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        double a,b,c;
+        System.out.println("enter the no.");
+        a = sc.nextDouble();
+        System.out.println("enter the no.");
+        b = sc.nextDouble();
+        c = a*b;
+        System.out.println(+c);
+
+    }
+
+}
